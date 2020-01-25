e8e65e3a90cf8dca6867130163640ebdb3f01757
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2020-01-25 23:13:06 +0200

    Added a few things

--- SolanumMod.cs
+++ SolanumMod.cs
@@ -1,4 +1,9 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace SolanumMod
 {
@@ -6,6 +11,23 @@ namespace SolanumMod
 	{
 		public SolanumMod()
 		{
+			Archeon.instance = this;
+			ModProperties properties = default(ModProperties);
+			properties.Autoload = true;
+			properties.AutoloadGores = true;
+			properties.AutoloadSounds = true;
+			properties.AutoloadBackgrounds = true;
+			base.Properties = properties;
+		}
+		
+		public override void AddRecipeGroups()
+		{
+			RecipeGroup recipeGroup = new RecipeGroup(() => "Gold or Platinum Bar", new int[]
+			{
+				19,
+				706
+			});
+			RecipeGroup.RegisterGroup("GoldOrPlatinumBar", recipeGroup);
 		}
 	}
 }
