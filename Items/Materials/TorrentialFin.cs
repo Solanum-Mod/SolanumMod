145386ecf03dbc84f12088dd036d296c8a00aa7a
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2020-01-25 17:13:41 +0200

    Add files via upload

---
+++ Items/Materials/TorrentialFin.cs
@@ -0,0 +1,18 @@
+﻿
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SolanumMod.Items.Materials
+{
+    class TorrentialFin : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 24;
+            item.maxStack = 999;
+            item.value = 100;
+            item.rare = 1;
+        }
+    }
+}
