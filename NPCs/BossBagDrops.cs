73340a9221b2c406b73fdb51e46a0a22adbed990
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2020-01-26 01:34:06 +0200

    NPCs stuff for Duke Fishron Torrential Fin drops

---
+++ NPCs/BossBagDrops.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SolanumMod.NPCs
+{
+	public class BossBagDrops : GlobalItem
+	{
+		public override void OpenVanillaBag(string context, Player player, int arg)
+		{
+			if (context == "bossBag" && arg == ItemID.FishronBossBag) {
+				player.QuickSpawnItem(mod.ItemType("TorrentialFin"), Main.rand.Next(14, 18));
+			}
+		}
+	}
+}
---
+++ NPCs/SolanumModGlobalNPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SolanumMod.NPCs
+{
+	public class SolanumModGlobalNPC : GlobalNPC
+	{
+		public override bool InstancePerEntity
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public override void NPCLoot(NPC npc)
+		{	
+			if (npc.type == 370) // Duke Fishron
+			{
+				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, base.mod.ItemType("TorrentialFin"), Main.rand.Next(13, 16), false, 0, false, false);
+			}
+		}
+	}
+}
