875358893d01dce64c789481586fb3eecc5fd1aa
Lemmy-Koopa <56778754+Lemmy-Koopa@users.noreply.github.com>
2020-03-21 21:40:55 -0400

    CrystalStuff

---
+++ CrystalShroomAxe.cs
@@ -0,0 +1,34 @@
+
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SolanumMod.Items.Weapons
+{
+	public class CrystalShroomAxe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("This is a modded pickaxe.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 8;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 23;
+			item.useAnimation = 23;
+			item.axe = 55;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+	}
+}
