f65fef007760952851e8e54b4bc3a1adf2031afa
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-10 12:08:10 +0100

    Add files via upload

---
+++ Projectiles/NothungP.cs
@@ -0,0 +1,77 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace SolanumMod.Projectiles
+{
+    class NothungP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 7;
+            projectile.height = 17;
+            projectile.timeLeft = 100;
+            projectile.penetrate = 1;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.aiStyle = 1;
+            projectile.scale = 1.2f;
+        }
+        public override void AI()
+        {
+            projectile.rotation = projectile.velocity.ToRotation() + (MathHelper.Pi / 2);
+            for (int i = 0; i < 200; i++)
+            {
+                NPC target = Main.npc[i];
+                //If the npc is hostile
+                if (!target.friendly)
+                {
+                    //Get the shoot trajectory from the projectile and target
+                    float shootToX = target.position.X + (float)target.width * 0.5f - projectile.Center.X;
+                    float shootToY = target.position.Y - projectile.Center.Y;
+                    float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+
+                    //If the distance between the live targeted npc and the projectile is less than 480 pixels
+                    if (distance < 480f && !target.friendly && target.active)
+                    {
+                        //Divide the factor, 3f, which is the desired velocity
+                        distance = 2.3f / distance;
+
+                        //Multiply the distance by a multiplier if you wish the projectile to have go faster
+                        shootToX *= distance * 5;
+                        shootToY *= distance * 5;
+
+                        //Set the velocities to the shoot values
+                        projectile.velocity.X = shootToX;
+                        projectile.velocity.Y = shootToY;
+                    }
+                }
+            }
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            for (int i = 0; i < 36; i++)
+            {
+                float angle = MathHelper.ToRadians(10 * i);
+                Vector2 vector = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                Dust dust = Dust.NewDustPerfect(projectile.Center + (vector * 40), 245, vector * 3f);
+                dust.noGravity = true;
+            }
+        }
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+            for (int k = 0; k < projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+                Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+                spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+            }
+            return true;
+        }
+    }
+}
