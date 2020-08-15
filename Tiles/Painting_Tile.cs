87d5523ca4480b6ec2cc793bde0bb4d5a19978a4
AlmightyOdin <65109915+AlmightyOdin@users.noreply.github.com>
2020-08-15 14:35:18 +0530

    Uploaded the 3 painting tiles

---
+++ Tiles/Painting_Tile.cs
@@ -0,0 +1,48 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+namespace SolanumMod.Tiles
+{
+    public class Painting_Tile : ModTile
+    {
+        public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+			TileObjectData.newTile.Height = 5;
+			TileObjectData.newTile.Width = 7;
+			TileObjectData.newTile.CoordinateHeights = new int[]
+			{
+				17,
+				17,
+				16,
+                16,
+                16
+			};
+			TileObjectData.newTile.AnchorBottom = default(AnchorData);
+			TileObjectData.newTile.AnchorTop = default(AnchorData);
+			TileObjectData.newTile.AnchorWall = true;
+			TileObjectData.addTile(Type);
+			disableSmartCursor = true;
+			dustType -= 1;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Painting");
+			AddMapEntry(new Color(10, 20, 255), name);
+		}
+
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 32, 48, ModContent.ItemType<Items.Placeables.Painting>());
+		}
+    }
+}
